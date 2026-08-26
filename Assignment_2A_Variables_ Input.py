8ed996b22bf51d48026f4f93e9c32ba84b36af24
Brent Marnati <29759055+flammensein@users.noreply.github.com>
2026-08-25 23:20:50 -0500

    First draft of the variable block and output.

--- Assignment_2A_Variables_ Input.py
+++ Assignment_2A_Variables_ Input.py
@@ -11,4 +11,19 @@ ASSIGNMENT REQUIREMENTS
 -----------------------------------------------------------------------
 """
 
-print("")
+# ℹ️ Gather information from the user
+pet_type = input("\nEnter the species of a pet: ")
+the_word_ultra = input("\nType the word 'ultra': ")
+sports_team = input("\nEnter the name of a sports team: ")
+fav_sport = input("\nWhat is your favorite sport?: ")
+score_num_one = input("\nPick a number: ")
+fish_descriptor = input("\nEnter an adjective: ")
+
+# ℹ️ Build a story using the aforementioned information provided by the user
+print(
+    f"\n\n\n\tThere once was a {pet_type} from Nantucket eating {fish_descriptor} fish from some buckets."
+)
+print(f"\n\tDown on the field, {sports_team} refused to yield.")
+print(f"\n\tThe {fav_sport} crowd cheered, what a-ruckus!")
+print(f"\n\tAt the end of the game, won by {score_num_one}, that's not lame.")
+print(f"\n\tA distraction the {the_word_ultra} rich gave us.\n\n\n")
