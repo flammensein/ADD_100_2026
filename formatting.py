77cafa15bfd1e2dee9ed433952641f33c3f7f3e6
Brent Marnati <29759055+flammensein@users.noreply.github.com>
2026-09-01 13:06:02 -0500

    Refactor formatting.py to uncomment expense calculations and user input prompts

--- formatting.py
+++ formatting.py
@@ -1,15 +1,33 @@
-col_rent = 1500
-col_utilities = 300
-col_phone = 55
+# col_rent = 1500
+# col_utilities = 300
+# col_phone = 55
 
-head_rent = "Rent"
-head_utilities = "Utilities"
-head_phone = "Phone"
+# head_rent = "Rent"
+# head_utilities = "Utilities"
+# head_phone = "Phone"
 
-expenses = col_phone + col_rent + col_utilities
-phone_percent = col_phone / expenses
+# expenses = col_phone + col_rent + col_utilities
+# phone_percent = col_phone / expenses
 
-# print(f"{head_rent:^20}{head_utilities:^20}{head_phone:^20}")
-# print(f"{col_rent:^20}{col_utilities:^20}{col_phone:^20}")
+# # print(f"{head_rent:^20}{head_utilities:^20}{head_phone:^20}")
+# # print(f"{col_rent:^20}{col_utilities:^20}{col_phone:^20}")
 
-print(f"\n\nPhone percent of expenses is:   {phone_percent:.2%}")
+# print(f"\n\nPhone percent of expenses is:   {phone_percent:.2%}")
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Ask user for Monthly Income (float).
+[ ] 3. Ask user for 5 DIFFERENT expense amounts (float)(Rent, Utilities, etc.)
+[ ] 4. Calculate Total Expenses and Remaining Balance.
+[ ] 5. Calculate Percentage of Income Spent.
+[ ] 6. Output formatted to 2 decimal places (:,.2f or :.2%).
+-----------------------------------------------------------------------
+"""
+
+monthly_income = float(input("\n\n\tPlease enter your monthly GROSS income:\t$   "))
+monthly_rent = float(input("\n\tPlease enter your monthly rent cost:\t$   "))
+monthly_utilities = float(input("\n\tPlease enter your monthly utilities:\t$   "))
+monthly_bills = float(input("\n\tPlease enter your monthly bills:\t$   "))
+monthly_debt = float(input("\n\tPlease enter your monthly debt payment:\t$   "))
