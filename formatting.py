31263bd32908c08f9951b8ff40a96264d503c281
Brent Marnati <29759055+flammensein@users.noreply.github.com>
2026-09-01 13:50:37 -0500

    Refactor formatting.py to implement user input for monthly expenses and display a formatted budget summary

--- formatting.py
+++ formatting.py
@@ -26,8 +26,35 @@ ASSIGNMENT REQUIREMENTS
 -----------------------------------------------------------------------
 """
 
-monthly_income = float(input("\n\n\tPlease enter your monthly GROSS income:\t$   "))
-monthly_rent = float(input("\n\tPlease enter your monthly rent cost:\t$   "))
-monthly_utilities = float(input("\n\tPlease enter your monthly utilities:\t$   "))
-monthly_bills = float(input("\n\tPlease enter your monthly bills:\t$   "))
-monthly_debt = float(input("\n\tPlease enter your monthly debt payment:\t$   "))
+# Get input for calculations ----
+gross_income = float(6686.20)
+monthly_rent = float(1220.50)
+monthly_utilities = float(200.00)
+monthly_bills = float(400)
+monthly_debt = float(2000)
+# gross_income = float(input("\n\n\tPlease enter your monthly GROSS income:\t$   "))
+# monthly_rent = float(input("\n\tPlease enter your monthly rent cost:\t$   "))
+# monthly_utilities = float(input("\n\tPlease enter your monthly utilities:\t$   "))
+# monthly_bills = float(input("\n\tPlease enter your monthly bills:\t$   "))
+# monthly_debt = float(input("\n\tPlease enter your monthly debt payment:\t$   "))
+# print("\n" + "=" * 90)
+print(f"{'=' * 90:^90}")
+print(f"{'Thank you! Your expense report/calculations are as follows:':^90}")
+print(f"{'=' * 90:^90}")
+
+# Perform budget calculations
+net_income = gross_income * 0.8
+tot_expenses = monthly_bills + monthly_debt + monthly_rent + monthly_utilities
+income_remaining = net_income - tot_expenses
+
+
+# Print the formatted report
+print(f"\n\n{'BASIC PERSONAL BUDGET (A SUMMARY)':^40}")
+print(
+    f"{'Gross Income':>15}{'Net Income':>15}{'Rent':>15}{'Utilities':>15}{'Other Bills':>15}{'Other Debt':>15}"
+)
+
+
+print(
+    f"{gross_income:>15,.2f}{net_income:>15,.2f}{monthly_rent:>15,.2f}{monthly_utilities:>15,.2f}{monthly_bills:>15,.2f}{monthly_debt:>15,.2f}\n"
+)
