d17cf2054bf4458a6c257671791394dcdfef3a37
Brent Marnati <29759055+flammensein@users.noreply.github.com>
2026-09-02 19:47:06 -0500

    Refactor formatting.py to improve comments for user input and calculation sections

--- formatting.py
+++ formatting.py
@@ -11,7 +11,8 @@ ASSIGNMENT REQUIREMENTS
 -----------------------------------------------------------------------
 """
 
-# Get input for calculations ----
+# Get input from the user ----
+# declaring variables as floats to ensure that decimal values are possible for calculations later
 gross_income = float(input("\n\n\tPlease enter your monthly GROSS income:\t$   "))
 monthly_rent = float(input("\n\tPlease enter your monthly rent cost:\t$   "))
 monthly_utilities = float(input("\n\tPlease enter your monthly utilities:\t$   "))
@@ -19,14 +20,14 @@ monthly_bills = float(input("\n\tPlease enter your monthly bills:\t$   "))
 monthly_debt = float(input("\n\tPlease enter your monthly debt payment:\t$   "))
 
 
-# Perform budget calculations
+# Perform budget calculations ----
 net_income = gross_income * 0.8
 tot_expenses = monthly_bills + monthly_debt + monthly_rent + monthly_utilities
 income_remaining = net_income - tot_expenses
 percent_income = float((tot_expenses / net_income))
 discretionary_income = float((income_remaining / net_income))
 
-# Print the formatted report
+# Print the formatted report ----
 print(f"\n\n")
 print(f"\t{'=' * 35:^35}")
 print(f"\t{'BASIC PERSONAL BUDGET':^35}")
