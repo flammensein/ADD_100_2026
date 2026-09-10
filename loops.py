80f9568ef184cc4e693843a063ebcaabd30a2d52
Brent Marnati <29759055+flammensein@users.noreply.github.com>
2026-09-10 13:32:18 -0500

    Enhance logic for bottle count in loops.py to handle edge cases and improve output clarity

--- loops.py
+++ loops.py
@@ -12,3 +12,43 @@ ASSIGNMENT REQUIREMENTS
 [ ] 4. Upload to GitHub and paste the link below.
 -----------------------------------------------------------------------
 """
+
+# NOTE: We haven't (as far as I can recall) Covered exiting the script/loop/logic intentionally, but
+# it's existed in every language I've ever used so I figured I'd try it and it seems to have worked...
+
+
+# parental_response = "no"
+# print(f"\n\tA child has decided to test your last nerve! Begin encounter...\n")
+# while parental_response.lower() != "yes":
+#     print(f"\n\t\tThe child asks: Are we there yet?")
+#     parental_response = input(f"\n\t\tHow do you respond?\t")
+
+# print(
+#     f"\n\n\tYou have acquiesced to the child's nagging or arrived at your destination. Good day.\n\n"
+# )
+
+
+for bottle_count in range(99, 0, -1):
+    if bottle_count > 1:
+        print(
+            f"\n\t{bottle_count} bottles of beer on the wall! {bottle_count} bottles of beer!!"
+        )
+        if (bottle_count - 1) == 1:
+            print(
+                f"\n\tTake one down! Pass it around! {bottle_count - 1} bottle of beer on the wall!!"
+            )
+        elif (bottle_count - 1) > 1:
+            print(
+                f"\n\tTake one down! Pass it around! {bottle_count - 1} bottles of beer on the wall!!"
+            )
+        else:
+            print(f"\n\tThere's something wrong with your wall...where's da beers?\n\n")
+            exit
+    elif bottle_count == 1:
+        print(
+            f"\n\t{bottle_count} bottle of beer on the wall! {bottle_count} bottle of beer!!"
+        )
+        print(
+            f"\n\tTake it down! Pass it around!! There're no bottles of beer on the wall!!!\n\n"
+        )
+        exit
